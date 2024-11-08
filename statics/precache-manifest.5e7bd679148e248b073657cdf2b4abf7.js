9f3c08db82b44d5d52e7c1077ccc0a625ad60967
wc <chong.wu.xiangfan@gmail.com>
2024-11-08 11:23:41 +0800

    feat: m4v,wmv格式视频允许在线预览

---
+++ statics/precache-manifest.5e7bd679148e248b073657cdf2b4abf7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0f3d9653707482366c2dc8bd11d9c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bea6eeca896905517b0c",
+    "url": "/static/css/6.e7f7aa76.chunk.css"
+  },
+  {
+    "revision": "348378837b5d0780e770",
+    "url": "/static/js/10.17a301c5.chunk.js"
+  },
+  {
+    "revision": "9ac12af42169dc6cc64d",
+    "url": "/static/js/11.e5c07ce2.chunk.js"
+  },
+  {
+    "revision": "7da28ac068e6785b5f79",
+    "url": "/static/js/12.a292d8e1.chunk.js"
+  },
+  {
+    "revision": "862f89d0a0bc1f552bfd",
+    "url": "/static/js/13.1c978c8f.chunk.js"
+  },
+  {
+    "revision": "bea6eeca896905517b0c",
+    "url": "/static/js/6.4e3686f2.chunk.js"
+  },
+  {
+    "revision": "e78feaf5b5a63fdc2e5d",
+    "url": "/static/js/7.3414f4b8.chunk.js"
+  },
+  {
+    "revision": "446e7c6a05d87350f3b9",
+    "url": "/static/js/8.686cf6d9.chunk.js"
+  },
+  {
+    "revision": "22db74cc661bd3ddd2a3",
+    "url": "/static/js/9.598b4ac4.chunk.js"
+  },
+  {
+    "revision": "ef32a73b8da8a7610916",
+    "url": "/static/js/codeEditor.7da39455.chunk.js"
+  },
+  {
+    "revision": "3c1a87568f2843186cd2",
+    "url": "/static/js/main.a18e07a6.chunk.js"
+  },
+  {
+    "revision": "285e44d6c695442455d5",
+    "url": "/static/js/runtime-main.2d7f3dbe.js"
+  },
+  {
+    "revision": "51c934afb61ee321ae0d9f05a4b432e9",
+    "url": "/static/media/getFetch.51c934af.cjs"
+  }
+]);

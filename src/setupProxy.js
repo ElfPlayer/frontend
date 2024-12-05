f09684a57c9bcbd239f975b1075afa35e3461732
ElfPlayer <xutop33@126.com>
2024-12-05 09:39:05 +0800

    chore: change proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
     ["/api", "/custom", "/f"].map(p => app.use(
         p,
         proxy({
-            // target: "http://localhost:5212",
-            target: "http://106.15.228.164:5212",
+            target: "http://localhost:5212",
+            // target: "http://106.15.228.164:5212",
             changeOrigin: true,
         })
     ))

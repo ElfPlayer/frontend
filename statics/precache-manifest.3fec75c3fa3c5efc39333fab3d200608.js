72aa3ea113f454e248b8011205224b0d8310cc6d
ElfPlayer <xutop33@126.com>
2025-08-26 10:57:30 +0800

    chore

---
+++ statics/precache-manifest.3fec75c3fa3c5efc39333fab3d200608.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "161c97aea64fe522298553506f366c68",
+    url: "/index.html",
+  },
+  {
+    revision: "8dc07f646f52e043b41a",
+    url: "/static/css/6.e7f7aa76.chunk.css",
+  },
+  {
+    revision: "348378837b5d0780e770",
+    url: "/static/js/10.17a301c5.chunk.js",
+  },
+  {
+    revision: "fd8a3883bbdb2c5bc8e1",
+    url: "/static/js/11.70286bd0.chunk.js",
+  },
+  {
+    revision: "65a4426ca478a50effb6",
+    url: "/static/js/12.ce78fcb1.chunk.js",
+  },
+  {
+    revision: "e45a2cb701704cb85b0c",
+    url: "/static/js/13.bc7e44a5.chunk.js",
+  },
+  {
+    revision: "8dc07f646f52e043b41a",
+    url: "/static/js/6.dc696e98.chunk.js",
+  },
+  {
+    revision: "e1e45f3e0d0cde637d6e",
+    url: "/static/js/7.2b7bb149.chunk.js",
+  },
+  {
+    revision: "446e7c6a05d87350f3b9",
+    url: "/static/js/8.686cf6d9.chunk.js",
+  },
+  {
+    revision: "22db74cc661bd3ddd2a3",
+    url: "/static/js/9.598b4ac4.chunk.js",
+  },
+  {
+    revision: "ef32a73b8da8a7610916",
+    url: "/static/js/codeEditor.7da39455.chunk.js",
+  },
+  {
+    revision: "773bb210857ed7dc2c04",
+    url: "/static/js/main.42b819de.chunk.js",
+  },
+  {
+    revision: "8bf925a5d1cadacb2f5a",
+    url: "/static/js/runtime-main.1fc03150.js",
+  },
+  {
+    revision: "51c934afb61ee321ae0d9f05a4b432e9",
+    url: "/static/media/getFetch.51c934af.cjs",
+  },
+]);
